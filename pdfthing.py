94d8908358adba4e17db010536d41034f6953c50
KefirAbsorber <jan.bogdanowicz05@gmail.com>
2026-04-12 17:08:42 +0200

    Added a way to select location of output file, changed names for clarity

--- pdfthing.py
+++ pdfthing.py
@@ -14,13 +14,15 @@ class MainWindow(QMainWindow):
         self.ui.list_files.setDragDropMode(QAbstractItemView.InternalMove)
         self.ui.list_files.setDefaultDropAction(Qt.MoveAction)
 
-        self.ui.button_addFile.clicked.connect(self.open_file_dialog)
+        self.ui.button_addFile.clicked.connect(self.file_dialog_input)
 
         self.ui.button_removeFile.clicked.connect(self.remove_file)
 
         self.ui.button_clearList.clicked.connect(self.clear_files)
 
-    def open_file_dialog(self):
+        self.ui.button_choseLocation.clicked.connect(self.file_dialog_output)
+
+    def file_dialog_input(self):
         file_paths, _ = QFileDialog.getOpenFileNames(self, 'Pick the pdf file', '', 'PDF files (*.pdf)')
 
         for path in file_paths:
@@ -34,6 +36,10 @@ class MainWindow(QMainWindow):
     def clear_files(self):
         self.ui.list_files.clear()
 
+    def file_dialog_output(self):
+        file_path, _ = QFileDialog.getSaveFileName(self,'Save Output PDF','','PDF Files (*.pdf);;All Files (*)')
+        if file_path:
+            self.ui.text_outputFile.setPlainText(file_path)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
